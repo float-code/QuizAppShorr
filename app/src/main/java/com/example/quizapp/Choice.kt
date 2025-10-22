c46f26a3119d9e757d8f80bdff34722d7cf117aa
pasha <pvoidevych@gmail.com>
2025-10-21 21:24:22 -0700

    update from home

---
+++ app/src/main/java/com/example/quizapp/Choice.kt
@@ -0,0 +1,5 @@
+package com.example.quizapp
+
+data class Choice(
+    val choice : Array<String>
+)
---
+++ app/src/main/java/com/example/quizapp/QuizTest.kt
@@ -0,0 +1,8 @@
+package com.example.quizapp
+
+data class QuizTest(
+    val questions : Array<String>,
+    val values : Array<Value>,
+    val choices : Array<Choice>
+)
+
